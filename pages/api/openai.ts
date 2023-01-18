b64c4ab2e276bc2925a04174105992467fb24006
M4T1SS3 <markorester@gmail.com>
2023-01-18 14:38:21 +0100

    1.11

--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -14,10 +14,10 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
   promt = "In the following I want you to act as an assistent called aid-e for victims of crime.   Comfort the person speaking to you, provide steps the victim can take next. Keep the situation and the context of the person you speak to in mind and refer your answer to the context. Answer in the language refered to you. Please answer directly as the assistent aid-e."
   //add strafgesetzt
 
-  if (first3 == "*!&") {
+  if (first3 === "*!&") {
    promt = promt + "this is the coversation history: " + req.body +  " Please provide the victim with a legal analysis based on the history where every violation is presented in the following format. Dont add other information that does not fit the asked format. Always use the following format, dont use numbers, labels for the input and input only where it is marked with ** (remove the ** when genereting the suggestions). Add at least 4 analyzed violations in this format to a list. This is the Format: violation: *here should be the name of the violoation of the law. Description: *here should be the description of the violation of the law*. Link: *here should be a link to further information about the crime*. "
 
-  } else if (first3 == "ö§-") {
+  } else if (first3 === "ö§-") {
     promt = promt + "this is the coversation history as a dialogue: " + req.body + " Please provide the victim with at least 4 general things the victim can do now to improve her situation. Be specific and refer the advice to the individual generation of the victim. Always use the following format, dont use numbers, labels for the input and input the text where it is marked with ** (remove the ** when genereting the suggestions). Add multiple suggestions in this format to a list. This is the format: headline *here should be the headline of the advice*. Description: *Here should be the description of the advice with detailed instructions and positive words*."
   }else {
     promt = promt + "this is the coversation history. " + req.body + " please provide an answer to the last message from [victim]: " 
