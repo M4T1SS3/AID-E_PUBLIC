93b1cf9c98be62519675a5b0d359588752a86e90
M4T1SS3 <markorester@gmail.com>
2023-01-22 17:48:40 +0100

    small fix

--- pages/blue/actionPlan.tsx
+++ pages/blue/actionPlan.tsx
@@ -11,7 +11,7 @@ export default function ActionPage() {
         try {
             let history = localStorage.getItem("messages")
             // https://aid-e.netlify.app
-            const response = await fetch(process.env.domain + '/api/openai', {
+            const response = await fetch('https://aid-e.netlify.app/api/openai', {
                 method: 'POST',
                 headers: {
                   'Content-Type': 'application/json',
